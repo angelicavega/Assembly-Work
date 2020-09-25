7f2bedb6cdc3ca3955680a41148307620a93adf2
Angélica M. Vega Aponte <av3387a@student.american.edu>
2020-09-24 21:47:58 -0400

    :construction:

---
+++ LED.asm
@@ -0,0 +1,51 @@
+int buttonState = 0;
+int fadeValue = 0;
+int delay1 =0;
+void setup() {
+// put your setup code here, to run once:
+// initialize the LED pin as an output:
+pinMode(ledPin, OUTPUT);
+// initialize the pushbutton pin as an input:
+pinMode(buttonPin, INPUT_PULLUP);
+}
+void loop() {
+// put your main code here, to run repeatedly:
+buttonState = digitalRead(buttonPin);
+if (buttonState == HIGH) {
+// turn LED on:
+digitalWrite(ledPin, HIGH); // fade in from min to max in increments of 5 points:
+for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += 5) {
+// sets the value (range from 0 to 255):
+analogWrite(ledPin, fadeValue);
+// wait for 30 milliseconds to see the dimming effect
+delay(20);
+}
+// fade out from max to min in increments of 5 points:
+for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= 5) {
+// sets the value (range from 0 to 255):
+analogWrite(ledPin, fadeValue);
+// wait for 30 milliseconds to see the dimming effect
+delay(20);
+}
+}
+else {
+// turn LED off:
+digitalWrite(ledPin, LOW);
+delay1 = (analogRead(sensorPin)/17);
+// fade in from min to max in increments of 5 points:
+for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += 5) {
+// sets the value (range from 0 to 255):
+analogWrite(ledPin, fadeValue);
+// wait for 30 milliseconds to see the dimming effect
+delay(delay1);
+}
+}
+// fade out from max to min in increments of 5 points:
+for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= 5) {
+// sets the value (range from 0 to 255):
+analogWrite(ledPin, fadeValue);
+// wait for 30 milliseconds to see the dimming effect
+delay(delay1);
+}
+}
+
