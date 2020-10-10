13284db54e2ffc8eef5585fa631494dccb8ab98a
Angélica M. Vega Aponte <av3387a@student.american.edu>
2020-10-09 23:04:16 -0400

    :construction:

--- LED.asm
+++ LED.asm
@@ -14,14 +14,14 @@ buttonState =         digitalRead(buttonPin);
 if (buttonState == HIGH) {
 // turn LED on:
 digitalWrite(ledPin, HIGH); // fade in from min to max in increments of 5 points:
-for (int fadeValue =  0; fadeValue <= 255; fadeValue += 5) {
+for (int fadeValue =  0, fadeValue <= 255; fadeValue += 5) {
 // sets the value (range from 0 to 255):
 analogWrite(ledPin, fadeValue);
 // wait for 30 milliseconds to see the dimming effect
 delay(20);
 }
 // fade out from max to min in increments of 5 points:
-for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= 5) {
+for (int fadeValue = 255, fadeValue >= 0, fadeValue -= 5) {
 // sets the value (range from 0 to 255):
 analogWrite(ledPin, fadeValue);
 // wait for 30 milliseconds to see the dimming effect
