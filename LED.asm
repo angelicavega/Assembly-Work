57a4518b06e1709272a0e89c91d6f16739b75ec4
Angélica M. Vega Aponte <av3387a@student.american.edu>
2020-10-17 23:44:42 -0400

    :construction:

--- LED.asm
+++ LED.asm
@@ -2,48 +2,48 @@ int buttonState =     0;
 int fadeValue =       0;
 int delay1 =          0;
 void setup() {
-// start the LED pin as an output:
+; // start the LED pin as an output:
 pinMode(ledPin, OUTPUT);
-// start the pushbutton pin as an input:
+; // start the pushbutton pin as an input:
 pinMode(buttonPin, INPUT_PULLUP);
 }
 void loop() {
-// main code goes here, to run repeatedly:
+; // start main code 
 buttonState =         digitalRead(buttonPin);
 if (buttonState == HIGH) {
 ; // turn LED on:
 digitalWrite(ledPin, HIGH); // fade in from min to max in increments of 5 points:
 for (int fadeValue =  0, fadeValue <= 255, fadeValue += 5) {
-// sets the value (range from 0 to 255):
+; // sets the value (range from 0 to 255):
 analogWrite(ledPin, fadeValue);
 // wait for 30 milliseconds to see the dimming effect
 delay(20);
 }
-// fade out from max to min in increments of 5 points:
+; // fade out from max to min in increments of 5 points:
 for (int fadeValue = 255, fadeValue >= 0, fadeValue -= 5) {
-// sets the value (range from 0 to 255):
+; // sets the value (range from 0 to 255):
 analogWrite(ledPin, fadeValue);
-// wait for 30 milliseconds to see the dimming effect
+; // wait for 30 milliseconds to see the dimming effect
 delay(20);
 }
 }
 else {
-// turn LED off:
+; // turn LED off:
 digitalWrite(ledPin, LOW);
 delay1 = (analogRead(sensorPin)/17);
-// fade in from min to max in increments of 5 points:
+; // fade in from min to max in increments of 5 points:
 for (int fadeValue = 0, fadeValue <= 255, fadeValue += 5) {
-// sets the value (range from 0 to 255):
+; // sets the value (range from 0 to 255):
 analogWrite(ledPin, fadeValue);
-// wait for 30 milliseconds to see the dimming effect
+; // wait for 30 milliseconds to see the dimming effect
 delay(delay1);
 }
 }
-// fade out from max to min in increments of 5 points:
+; // fade out from max to min in increments of 5 points:
 for (int fadeValue = 255, fadeValue >= 0, fadeValue -= 5) {
-// sets the value (range from 0 to 255):
+; // sets the value (range from 0 to 255):
 analogWrite(ledPin, fadeValue);
-// wait for 30 milliseconds to see the dimming effect
+; // wait for 30 milliseconds to see the dimming effect
 delay(delay1);
 }
 }
